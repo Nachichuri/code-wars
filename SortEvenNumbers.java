0c628c71e7f96d1084e337ee1669d1058b556784
Nachichuri <nachichuri@gmail.com>
2020-06-01 16:13:13 -0300

    Nuevos ejercicios

---
+++ SortEvenNumbers.java
@@ -0,0 +1,49 @@
+package com.codewars;
+
+// You have an array of numbers.
+// Your task is to sort ascending odd numbers but even numbers must be on their places.
+// Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+//
+// Example
+// sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortEvenNumbers {
+    public static int[] sortArray(int[] array) {
+        ArrayList<Integer> oddNumbers = new ArrayList<>();
+        ArrayList<Byte> indexes = new ArrayList<>();
+
+        if (array.length == 0) {
+            return array;
+        } else {
+            for (byte i = 0; i < array.length; i++) {
+                if (array[i] % 2 != 0) {
+                    oddNumbers.add(array[i]);
+                    indexes.add(i);
+                }
+            }
+            // Ordenamos el ArrayList con los enteros pares
+            Collections.sort(oddNumbers);
+            // Modificamos el array
+            for (byte i = 0; i < indexes.toArray().length; i++)
+                array[indexes.get(i)] = oddNumbers.get(i);
+            return array;
+        }
+    }
+}
+
+// Solución más votada en CodeWars, un poco más elegante usando una stream:
+//   public static int[] sortArray(final int[] array) {
+//
+//    // Sort the odd numbers only
+//    final int[] sortedOdd = Arrays.stream(array).filter(e -> e % 2 == 1).sorted().toArray();
+//
+//    // Then merge them back into original array
+//    for (int j = 0, s = 0; j < array.length; j++) {
+//      if (array[j] % 2 == 1) array[j] = sortedOdd[s++];
+//    }
+//
+//    return array;
+//  }
---
+++ TwoToOne.java
@@ -0,0 +1,40 @@
+package com.codewars;
+
+// Take 2 strings s1 and s2 including only letters from ato z.
+// Return a new sorted string, the longest possible, containing distinct letters, each taken only once - coming from s1 or s2.
+//
+// Example:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+//
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+import java.util.stream.Collectors;
+
+public class TwoToOne {
+
+    public static String longest(String s1, String s2) {
+        // Streams FTW
+        String stringsUnidas = s1 + s2;
+
+        String letrasUnicas = stringsUnidas.chars()
+                .distinct()
+                .mapToObj(c -> String.valueOf((char) c))
+                .collect(Collectors.joining());
+
+        String stringOrdenada = letrasUnicas.chars()
+                .sorted()
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        return stringOrdenada;
+    }
+}
+
+// Mejor solucion de CodeWars
+// public static String longest (String s1, String s2) {
+//        String s = s1 + s2;
+//        return s.chars().distinct().sorted().collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+//    }
