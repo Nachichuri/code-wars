f31e35841e4ba633738cff27cb850d9ae68e7458
Nachichuri <nachichuri@gmail.com>
2020-06-10 19:59:01 -0300

    Yet another one

---
+++ ExpandedForm.java
@@ -0,0 +1,23 @@
+package com.codewars;
+
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example://
+// Kata.expandedForm(42); # Should return "40 + 2"
+// Kata.expandedForm(70304); # Should return "70000 + 300 + 4"
+
+public class ExpandedForm {
+    public static String expandedForm(int num) {
+        String resultado = "";
+
+        for (int i = 10; i < num; i *= 10) {
+            int resto = num % i;
+            resultado = (resto > 0) ? " + " + resto + resultado : resultado; // I love you ternary operator
+            num -= resto;
+        }
+        resultado = num + resultado;
+
+        return resultado;
+    }
+}
+
+// Tarea para otro dia: Hacer con Streams
