f41b55087dc492ca9d68629ac6361b49700eea72
Nachichuri <nachichuri@gmail.com>
2020-06-17 19:59:31 -0300

    Rainy wednesday

---
+++ WhereIsMyParent.java
@@ -0,0 +1,32 @@
+package com.codewars;
+// Mothers arranged a dance party for the children in school. At that party, there are only mothers and their children.
+// All are having great fun on the dance floor when suddenly all the lights went out. It's a dark night and no one can see each other.
+// But you were flying nearby and you can see in the dark and have ability to teleport people anywhere you want.
+// Legend:
+// -Uppercase letters stands for mothers, lowercase stand for their children, i.e. "A" mother's children are "aaaa".
+// -Function input: String contains only letters, uppercase letters are unique.
+// Task:
+// Place all people in alphabetical order where Mothers are followed by their children, i.e. "aAbaBb" => "AaaBbb".
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WhereIsMyParent {
+    static String findChildren(final String text) {
+        // Streams baby!
+        List<String> items = text.chars()
+                .mapToObj(c -> String.valueOf((char) c))
+                .sorted().sorted(String.CASE_INSENSITIVE_ORDER)
+                .collect(Collectors.toList());
+
+        return String.join("", items);
+    }
+}
+
+// Mejor solución en CodeWars:
+//  static String findChildren(final String text) {
+//        String[] sorted = text.split("");
+//        Arrays.sort(sorted);
+//        Arrays.sort(sorted, String.CASE_INSENSITIVE_ORDER);
+//        return String.join("", sorted);
+//    }
