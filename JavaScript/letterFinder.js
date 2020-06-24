55cf0220eb9dccb548c36767d25c3de2e9a43ec9
Nachichuri <nachichuri@gmail.com>
2020-06-24 17:54:39 -0300

    Rainy Wednesday

---
+++ JavaScript/letterFinder.js
@@ -0,0 +1,26 @@
+// #Find the missing letter
+
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+// You will always get a valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+// Example:
+// ['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
+
+function findMissingLetter(array) {
+  // Se puede resolver usando el codigo de cada char
+  // Pasamos a un String para poder iterar sobre los chars
+  var arrayString = array.join("");
+  // Restamos el unicode de cada item al item siguiente, y en caso de que no sea el sigueinte devolvemos como stirng el char correpsondiente
+  for (let i = 0; i < arrayString.length; i++)
+    if (arrayString.charCodeAt(i + 1) - arrayString.charCodeAt(i) != 1)
+      return String.fromCharCode(arrayString.charCodeAt(i) + 1);
+}
+
+console.log(findMissingLetter(["a", "b", "c", "d", "f"]));
+
+// Mejor respuesta en CodeWars:
+// Un poco más elegante:
+// var i=array[0].charCodeAt();
+// array.map(x=>  x.charCodeAt()==i?i++:i);
+// return String.fromCharCode(i);
